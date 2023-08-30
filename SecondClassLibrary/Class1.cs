b376e2a00f9724a19570b47b63c718d8c01a0123
Nikita Stepanov <stepanov@mego.travel>
2023-08-30 18:13:26 +0300

    йцу

--- SecondClassLibrary/Class1.cs
+++ SecondClassLibrary/Class1.cs
@@ -5,5 +5,7 @@ namespace SecondClassLibrary
     public class Class2
     {
         public string GetName() => typeof(Class2).Name;
+
+        public string GetSecondName() => typeof(Class2).Name;
     }
 }
